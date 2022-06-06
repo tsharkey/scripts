07a586f7afcf67a208b15edfb01dca42cd585803
Tom Sharkey <sharkey.thomasm@gmail.com>
2022-06-05 23:12:07 -0400

    feat: support getting tickets from jira for branch names

---
+++ pyutils/jirahelpers.py
@@ -0,0 +1,22 @@
+import os
+
+import jira
+
+
+def get_user_tickets(username, jira_api_token=os.getenv('JIRA_API_TOKEN')):
+    """get_user_tickets
+    Returns a list of tickets that the user is assigned to.
+    """
+
+    project_query = 'project = "DEV"'
+
+    status_query = ('status in ("Backlog", "In Progress",'
+                    '"Ready to Deploy", "Code Review", "Ready to Test", "To Do")')
+
+    assignee_query = f'assignee = "{username}"'
+
+    j = jira.JIRA("https://promoboxx.atlassian.net",
+                  basic_auth=(username, jira_api_token))
+
+    full_query = f'{project_query} AND {status_query} AND {assignee_query}'
+    return j.search_issues(full_query)
